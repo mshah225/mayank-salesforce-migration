6fe7c0c291143337b5ee327d40cdbf71f58425e9
Keith Arnold <32712024+keithiarnold@users.noreply.github.com>
2018-12-03 14:41:18 -0700

    Added link function, clear function, and button disable

--- src/aura/JiraReportIssue/JiraReportIssueHelper.js
+++ src/aura/JiraReportIssue/JiraReportIssueHelper.js
@@ -1,5 +1,8 @@
 ({
     submitIssueForm : function (component) {
+        var submitButton = component.find("submitButton");
+        submitButton.set("v.disabled", true);
+        
         var title = component.get("v.title");
         var questionOne = component.get("v.questionOne");
         var questionTwo = component.get("v.questionTwo");
@@ -26,12 +29,28 @@
         if (state === "SUCCESS") {
             var key = response.getReturnValue();
             component.set("v.message","Jira Issue Successfully Created");
-            var link = "Created Issue: https://asudev.jira.com/browse/" + key;
+            var link = "https://asudev.jira.com/browse/" + key;
             component.set("v.link", link);
+            this.clearInputs(component);
         } else {
             component.set("v.message","Jira Issue could not be created!");
         }
+        submitButton.set("v.disabled", false);
       });
       $A.enqueueAction(action);
+	},
+    
+    clearInputs : function (component) {
+        component.set("v.title", "");
+        component.set("v.answerOne", "");
+        component.set("v.answerTwo", "");
+        component.set("v.answerThree", "");
+        component.set("v.watchers", "");
+    },
+    
+    navigateToIssue : function (component) {
+    	var navigate = $A.get("e.force:navigateToURL");
+        navigate.setParams({"url" : component.get("v.link")});
+    	navigate.fire();
 	}
 })

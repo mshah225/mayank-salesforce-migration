66657659fb4db4f233016be122546deda08b9954
Keith Arnold <32712024+keithiarnold@users.noreply.github.com>
2018-12-03 14:41:48 -0700

    Added navigate function

--- src/aura/JiraReportIssue/JiraReportIssueController.js
+++ src/aura/JiraReportIssue/JiraReportIssueController.js
@@ -1,5 +1,9 @@
 ({
     submitIssueForm : function (component, event, helper) {
         helper.submitIssueForm(component);
+	},
+    
+    navigateToIssue: function(component, event, helper) {
+	    helper.navigateToIssue(component);
 	}
 })

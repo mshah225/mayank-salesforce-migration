64441a8aadb761800ecceff8eaf67ab311586f94
Keith Arnold <32712024+keithiarnold@users.noreply.github.com>
2018-12-04 13:51:17 -0700

    Update JiraViewAllIssuesHelper.js

--- src/aura/JiraViewAllIssues/JiraViewAllIssuesHelper.js
+++ src/aura/JiraViewAllIssues/JiraViewAllIssuesHelper.js
@@ -15,5 +15,30 @@
             component.set("v.myWatchedIssues", actionResult.getReturnValue());
         });
         $A.enqueueAction(action);   
+    },
+    
+    handleStatusChange : function(component, event) {
+        var issueKey = event.getParam("issueKey");
+        var status = event.getParam("status");
+        
+        var myIssues = component.get("v.myIssues");
+        myIssues.forEach(function(issue) {
+            console.log(issue);
+            if (issue.issueId == issueKey) {
+                console.log(status);
+                issue.status = status;
+                component.set("v.myIssues", myIssues);
+            }
+        });
+        
+        var myWatchedIssues = component.get("v.myWatchedIssues");
+        myWatchedIssues.forEach(function(issue) {
+            console.log(issue);
+            if (issue.issueId == issueKey) {
+                console.log(status);
+                issue.status = status;
+                component.set("v.myWatchedIssues", myWatchedIssues);
+            }
+        });
     }
 })

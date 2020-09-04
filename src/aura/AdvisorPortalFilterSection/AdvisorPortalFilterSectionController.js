35041a443db9fac818eb40ac062ae82cee5443f9
Robert Nordman <rnordman@asu.edu>
2020-09-04 16:06:47 -0700

    Further cap picklist queries.

--- src/aura/AdvisorPortalFilterSection/AdvisorPortalFilterSectionController.js
+++ src/aura/AdvisorPortalFilterSection/AdvisorPortalFilterSectionController.js
@@ -4,8 +4,8 @@
     },
 
     toggleFilterListPanel: function (component, event, helper) {
-        component.set("v.isFilterSectionShown", !component.get("v.isFilterSectionShown"));
-        helper.handleToggleFilterButtons(component, event, helper, "FilterToggleState", "filtersToggle");
+        component.set('v.isFilterSectionShown', !component.get('v.isFilterSectionShown'));
+        helper.handleToggleFilterButtons(component, event, helper, 'FilterToggleState', 'filtersToggle');
     },
 
     applyFilters: function (component, event, helper) {
@@ -13,31 +13,31 @@
     },
 
     clearFilters: function (component, event, helper) {
-        component.set("v.Student", "");
-        component.find("campusPicklist").resetMultiSelect();
-        component.set("v.Major", "");
-        component.find("residencyPicklist").resetMultiSelect();
-        component.find("caseStatusPicklist").resetMultiSelect();
-        component.find("caseSubjectPicklist").resetMultiSelect();
-        component.find("caseCategoryPicklist").resetMultiSelect();
-        component.find("academicLevelPicklist").resetMultiSelect();
-        component.set("v.Advisor", "");
-        component.set("v.StudentGroup", "");
-        component.find("outlookScoreOptionsPicklist").resetMultiSelect();
-        component.find("outlookChangePicklist").resetMultiSelect();
-        component.set("v.CaseCount", null);
-        component.set("v.CreatedFromDate", "");
-        component.set("v.CreatedToDate", "");
-        component.set("v.FollowUpFromDate", "");
-        component.set("v.FollowUpToDate", "");
-        component.set("v.PersistenceFromDate", "");
-        component.set("v.PersistenceToDate", "");
-        component.set("v.AllCasesState", false);
-        component.find("allCasesFilter").set("v.variant", "neutral");
-        component.set("v.ProactiveCasesState", true);
-        component.find("proactiveFilter").set("v.variant", "brand");
-        component.set("v.WatchlistCasesState", false);
-        component.find("watchlistFilter").set("v.variant", "neutral");
+        component.set('v.Student', '');
+        component.find('campusPicklist').resetMultiSelect();
+        component.set('v.Major', '');
+        component.find('residencyPicklist').resetMultiSelect();
+        component.find('caseStatusPicklist').resetMultiSelect();
+        component.find('caseSubjectPicklist').resetMultiSelect();
+        component.find('caseCategoryPicklist').resetMultiSelect();
+        component.find('academicLevelPicklist').resetMultiSelect();
+        component.set('v.Advisor', '');
+        component.set('v.StudentGroup', '');
+        component.find('outlookScoreOptionsPicklist').resetMultiSelect();
+        component.find('outlookChangePicklist').resetMultiSelect();
+        component.set('v.CaseCount', null);
+        component.set('v.CreatedFromDate', '');
+        component.set('v.CreatedToDate', '');
+        component.set('v.FollowUpFromDate', '');
+        component.set('v.FollowUpToDate', '');
+        component.set('v.PersistenceFromDate', '');
+        component.set('v.PersistenceToDate', '');
+        component.set('v.AllCasesState', false);
+        component.find('allCasesFilter').set('v.variant', 'neutral');
+        component.set('v.ProactiveCasesState', true);
+        component.find('proactiveFilter').set('v.variant', 'brand');
+        component.set('v.WatchlistCasesState', false);
+        component.find('watchlistFilter').set('v.variant', 'neutral');
         helper.applyFilters(component, true);
     },
 
@@ -52,15 +52,15 @@
     },
 
     handleAllCasesState: function (component, event, helper) {
-        helper.handleToggleFilterButtons(component, event, helper, "", "allCasesFilter");
+        helper.handleToggleFilterButtons(component, event, helper, '', 'allCasesFilter');
     },
 
     handleProactiveCasesState: function (component, event, helper) {
-        helper.handleToggleFilterButtons(component, event, helper, "", "proactiveFilter");
+        helper.handleToggleFilterButtons(component, event, helper, '', 'proactiveFilter');
     },
 
     handleWatchlistCasesState: function (component, event, helper) {
-        helper.handleToggleFilterButtons(component, event, helper, "", "watchlistFilter");
+        helper.handleToggleFilterButtons(component, event, helper, '', 'watchlistFilter');
     },
 
     validateDates: function (component, event, helper) {
@@ -68,22 +68,21 @@
     },
 
     buildUserIds: function (component, event, helper) {
-        let userIds = component.get("v.SelectedViewAsUserOptions").map(option => option.value);
-        component.set("v.UserIds", userIds);
+        let userIds = component.get('v.SelectedViewAsUserOptions').map((option) => option.value);
+        component.set('v.UserIds', userIds);
 
-        let caseSubjectPicklistAction = component.get("c.getCaseSubjectPicklistValues");
-        caseSubjectPicklistAction.setParams({ viewAsOptions: userIds });
+        let caseSubjectPicklistAction = component.get('c.getCaseSubjectPicklistValues');
+        caseSubjectPicklistAction.setParams({viewAsOptions: userIds});
         caseSubjectPicklistAction.setCallback(this, function (response) {
-            component.set("v.CaseSubjectPicklistValues", helper.buildPicklistOptionsArray(response.getReturnValue()));
+            component.set('v.CaseSubjectPicklistValues', helper.buildPicklistOptionsArray(response.getReturnValue()));
         });
         $A.enqueueAction(caseSubjectPicklistAction);
 
-        let caseClassificationPicklistAction = component.get("c.getCaseClassificationPicklistValues");
-        caseClassificationPicklistAction.setParams({ viewAsOptions: userIds });
+        let caseClassificationPicklistAction = component.get('c.getCaseClassificationPicklistValues');
+        caseClassificationPicklistAction.setParams({viewAsOptions: userIds});
         caseClassificationPicklistAction.setCallback(this, function (response) {
-            component.set("v.CaseCategoryPicklistValues", helper.buildPicklistOptionsArray(response.getReturnValue()));
+            component.set('v.CaseCategoryPicklistValues', helper.buildPicklistOptionsArray(response.getReturnValue()));
         });
         $A.enqueueAction(caseClassificationPicklistAction);
-
     },
-})
+});

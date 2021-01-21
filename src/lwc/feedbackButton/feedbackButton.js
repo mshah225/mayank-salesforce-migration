ac126a4e58791c49d6c985835b65e6a81c64490c
Robert Nordman <rnordman@asu.edu>
2021-01-21 12:40:32 -0700

    Backend of LWC Feedback button and integrate front and back

--- src/lwc/feedbackButton/feedbackButton.js
+++ src/lwc/feedbackButton/feedbackButton.js
@@ -1,8 +1,9 @@
 import { LightningElement, api } from 'lwc';
+import submitFeedback from '@salesforce/apex/FeedbackButtonService.submitFeedback';
 
 export default class FeedbackButton extends LightningElement {
     @api iconSize = 'xx-small';
-    showError = false;
+    @api carName;
 
     openModal() {
         this.template.querySelector('.slds-modal').classList.add('slds-fade-in-open');
@@ -18,12 +19,26 @@ export default class FeedbackButton extends LightningElement {
         if (this.template.querySelector('lightning-textarea').value) {
             feedback = this.template.querySelector('lightning-textarea').value.trim();
         }
-        if (feedback && feedback.length > 0) {
-            console.log(feedback);
-            this.showError = false;
+        if (feedback.length > 0) {
+            submitFeedback({ carName: this.carName, feedbackText: feedback })
+                .then(result => {
+                    this.makeToast('success', 'Success', 'Feedback successfully submitted');
+                })
+                .catch(error => {
+                    if (error.body.message.includes('Our support team has been notified of this error. If you require immediate assistance please call 1-855-ASU-5080')) {
+                        this.makeToast('error', 'Error', error.body.message);
+                    } else {
+                        this.makeToast('error', 'Error', error.body.message+'. Our support team has been notified of this error. If you require immediate assistance please call 1-855-ASU-5080');
+                    }
+                });
         } else {
-            console.log('EMPTY!!');
-            this.showError = true;
+            this.makeToast('error','Error','Feedback must contain some content.')
         }
+    }
+
+    makeToast(type, title, body) {
+        console.log(type);
+        console.log(title);
+        console.log(body);
     }
 }

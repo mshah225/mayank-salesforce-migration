ec3b5f53884277efe1043cbed2fcce970a460502
Michael Gilardi <mgilardi@asu.edu>
2021-01-26 20:09:14 -0700

    Additional formatting for update script

--- update-branches.sh
+++ update-branches.sh
@@ -6,10 +6,12 @@ message="Hello there, traveler."; for ((i=0; i<${#message}; i++)); do echo "afte
 sleep 1
 message=" I'm Alius."; for ((i=0; i<${#message}; i++)); do echo "after 30" | tclsh; printf "${message:$i:1}"; done; echo;
 sleep 1
+printf "\n"
 message="You look as though you have wondrous tales to share."; for ((i=0; i<${#message}; i++)); do echo "after 30" | tclsh; printf "${message:$i:1}"; done; echo;
 sleep 1
 message="Unfortunately, I was not created with the ability to listen to them."; for ((i=0; i<${#message}; i++)); do echo "after 30" | tclsh; printf "${message:$i:1}"; done; echo;
 sleep 1
+printf "\n"
 message="I trust that you have not come with devious intent."; for ((i=0; i<${#message}; i++)); do echo "after 30" | tclsh; printf "${message:$i:1}"; done; echo;
 sleep 1
 message="Shall we begin? (y/n) "; for ((i=0; i<${#message}; i++)); do echo "after 30" | tclsh; printf "${message:$i:1}"; done; echo -ne;
@@ -80,12 +82,14 @@ for branch in $(git for-each-ref --format='%(refname:short)' --sort='*refname:sh
     fi
     printf "\n"
 done
-message="==================================="; for ((i=0; i<${#message}; i++)); do echo "after 5" | tclsh; printf "${message:$i:1}"; done; echo;
-message=">>> Number of successful pushes: "; for ((i=0; i<${#message}; i++)); do echo "after 5" | tclsh; printf "\033[1;32m${message:$i:1}\033[0m"; done; echo -ne;
+printf "\n"
+message="==========================="; for ((i=0; i<${#message}; i++)); do echo "after 5" | tclsh; printf "${message:$i:1}"; done; echo;
+message=">>> Number of successes: "; for ((i=0; i<${#message}; i++)); do echo "after 5" | tclsh; printf "\033[1;32m${message:$i:1}\033[0m"; done; echo -ne;
 message="$successCount"; for ((i=0; i<${#message}; i++)); do echo "after 5" | tclsh; printf "${message:$i:1}"; done; echo;
-message=">>> Number of failed merges: $failureCount"; for ((i=0; i<${#message}; i++)); do echo "after 5" | tclsh; printf "\033[1;31m${message:$i:1}\033[0m"; done; echo -ne;
+message=">>> Number of failures:  "; for ((i=0; i<${#message}; i++)); do echo "after 5" | tclsh; printf "\033[1;31m${message:$i:1}\033[0m"; done; echo -ne;
 message="$failureCount"; for ((i=0; i<${#message}; i++)); do echo "after 5" | tclsh; printf "${message:$i:1}"; done; echo;
-message="==================================="; for ((i=0; i<${#message}; i++)); do echo "after 5" | tclsh; printf "${message:$i:1}"; done; echo;
+message="==========================="; for ((i=0; i<${#message}; i++)); do echo "after 5" | tclsh; printf "${message:$i:1}"; done; echo;
+printf "\n"
 
 # Switch back to master because it looks cleaner at the end
 git checkout master &>/dev/null

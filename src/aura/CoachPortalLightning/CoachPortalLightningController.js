aec6e945fb8d000eb76b26e1d898998c96009734
Michael Gilardi <mgilardi@asu.edu>
2021-02-13 04:13:05 -0700

    Formatting fixes for LightningConversion

--- src/aura/CoachPortalLightning/CoachPortalLightningController.js
+++ src/aura/CoachPortalLightning/CoachPortalLightningController.js
@@ -1,27 +1,33 @@
 ({
     doInit: function (component) {
-        var workspaceAPI = component.find("workspace");
-        window.addEventListener("message", $A.getCallback(function (event) {
-            var validOrigin = false;
-            var messageValid = false;
-            // Catchall to ensure coming from asu URL
-            if (event.origin.substring(0, 11) == "https://asu") {
-                validOrigin = true;
-            }
-            // List of valid messages
-            if (event.data.includes("/500/e?retURL=%2F0036300000AU5YZ&def_contact_id=")
-                || event.data.includes("/lightning/r/Case/")
-                || event.data.includes("/apex/successRecoding")) {
-                messageValid = true;
-            }
-            if (validOrigin && messageValid) {
-                workspaceAPI.openTab({
-                    url: event.data,
-                    focus: true
-                });
-            } else {
-                return;
-            }
-        }), false);
-    }
-})
+        var workspaceAPI = component.find('workspace');
+        window.addEventListener(
+            'message',
+            $A.getCallback(function (event) {
+                var validOrigin = false;
+                var messageValid = false;
+                // Catchall to ensure coming from asu URL
+                if (event.origin.substring(0, 11) == 'https://asu') {
+                    validOrigin = true;
+                }
+                // List of valid messages
+                if (
+                    event.data.includes('/500/e?retURL=%2F0036300000AU5YZ&def_contact_id=') ||
+                    event.data.includes('/lightning/r/Case/') ||
+                    event.data.includes('/apex/successRecoding')
+                ) {
+                    messageValid = true;
+                }
+                if (validOrigin && messageValid) {
+                    workspaceAPI.openTab({
+                        url: event.data,
+                        focus: true,
+                    });
+                } else {
+                    return;
+                }
+            }),
+            false
+        );
+    },
+});

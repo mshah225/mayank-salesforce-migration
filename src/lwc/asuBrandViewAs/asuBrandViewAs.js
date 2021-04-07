38c0481d4b68e9d30c6d2b8353b9b1afbf054c4b
Michael Gilardi <mgilardi@asu.edu>
2021-04-07 10:54:23 -0700

    View as LWC updates

--- src/lwc/asuBrandViewAs/asuBrandViewAs.js
+++ src/lwc/asuBrandViewAs/asuBrandViewAs.js
@@ -1,4 +1,4 @@
-import {LightningElement, api} from 'lwc';
+import {LightningElement, api, track} from 'lwc';
 
 export default class AsuBrandViewAs extends LightningElement {
     @api firstName;
@@ -7,4 +7,17 @@ export default class AsuBrandViewAs extends LightningElement {
     @api emplId;
     @api stopViewAsUrl;
     @api viewAsUrl;
+
+    @track stop;
+
+    connectedCallback() {
+        this.stop = 'Logout as ' + this.firstName + ' ' + this.lastName;
+        console.log(this.stop);
+        console.log(this.firstName);
+        console.log(this.lastName);
+        console.log(this.viewingAs);
+        console.log(this.emplId);
+        console.log(this.stopViewAsUrl);
+        console.log(this.viewAsUrl);
+    }
 }

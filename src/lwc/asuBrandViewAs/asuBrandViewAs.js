fab86021d266a294174d4cb04536ca8f6cf124ec
Michael Gilardi <mgilardi@asu.edu>
2021-04-13 16:17:35 -0700

    View as updates

--- src/lwc/asuBrandViewAs/asuBrandViewAs.js
+++ src/lwc/asuBrandViewAs/asuBrandViewAs.js
@@ -3,21 +3,24 @@ import {LightningElement, api, track} from 'lwc';
 export default class AsuBrandViewAs extends LightningElement {
     @api firstName;
     @api lastName;
-    @api viewingAs;
     @api emplId;
-    @api stopViewAsUrl;
+    @api viewingAs;
     @api viewAsUrl;
+    @api stopViewAsUrl;
 
-    @track stop;
+    @track viewAsUrlText;
+    @track stopViewAsUrlText;
 
     connectedCallback() {
-        this.stop = 'Logout as ' + this.firstName + ' ' + this.lastName;
-        console.log(this.stop);
+        this.viewAsUrlText = 'View as someone else';
+        this.stopViewAsUrlText = 'Logout as ' + this.firstName + ' ' + this.lastName;
+        console.log(this.viewAsUrlText);
+        console.log(this.stopViewAsUrlText);
         console.log(this.firstName);
         console.log(this.lastName);
-        console.log(this.viewingAs);
         console.log(this.emplId);
-        console.log(this.stopViewAsUrl);
+        console.log(this.viewingAs);
         console.log(this.viewAsUrl);
+        console.log(this.stopViewAsUrl);
     }
 }

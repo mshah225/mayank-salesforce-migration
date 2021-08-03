fdff1e4add3df4355479d92d384525f37b7bcd35
Michael Gilardi <mgilardi@asu.edu>
2021-08-02 20:16:49 -0700

    SFE-36996: Create a Web Standards compliant alert

--- force-app/main/default/aura/ASUBrandApp/ASUBrandApp.app
+++ force-app/main/default/aura/ASUBrandApp/ASUBrandApp.app
@@ -2,4 +2,5 @@
     <aura:dependency resource="c:asuBrandHeader" />
     <aura:dependency resource="c:asuBrandFooter" />
     <aura:dependency resource="c:asuBrandViewAs" />
+    <aura:dependency resource="c:asuBrandAlert" />
 </aura:application>
--- force-app/main/default/lwc/asuBrandViewAs/asuBrandViewAs.js
+++ force-app/main/default/lwc/asuBrandViewAs/asuBrandViewAs.js
@@ -14,13 +14,12 @@ export default class AsuBrandViewAs extends LightningElement {
     connectedCallback() {
         this.viewAsUrlText = 'View as someone else';
         this.stopViewAsUrlText = 'Logout as ' + this.firstName + ' ' + this.lastName;
-        console.log(this.viewAsUrlText);
-        console.log(this.stopViewAsUrlText);
-        console.log(this.firstName);
-        console.log(this.lastName);
-        console.log(this.emplId);
-        console.log(this.viewingAs);
-        console.log(this.viewAsUrl);
-        console.log(this.stopViewAsUrl);
+
+        console.log('--- VIEW AS [BEGIN] ---');
+        console.log('Logged in as ' + this.firstName + ' ' + this.lastName + ' (' + this.emplId + ')');
+        console.log('Permission to view as: ' + this.viewingAs);
+        console.log('View As URL: ' + this.viewAsUrl);
+        console.log('Stop View As URL: ' + this.stopViewAsUrl);
+        console.log('--- VIEW AS [END] ---');
     }
 }

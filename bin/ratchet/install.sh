c8b7499ec2617d3a97db346bf39c1c9efd397e29
Robert Nordman <rnordman@asu.edu>
2022-04-25 10:02:52 -0700

    ratchet fixes

--- bin/ratchet/install.sh
+++ bin/ratchet/install.sh
@@ -2,6 +2,6 @@
 # Git directory
 rootDir=$(git rev-parse --show-toplevel)
 # Add to pre-commit workflow
-sed -i '' 's/. "\$(dirname "\$0")\/husky.sh"/. "\$(dirname "\$0")\/ratchet.sh"\n. "\$(dirname "\$0")\/husky.sh"/g' $rootDir/.git/hooks/pre-commit
+egrep "ratchet.sh" $rootDir/.git/hooks/pre-commit >/dev/null || sed -i '' 's/. "\$(dirname "\$0")\/husky.sh"/. "\$(dirname "\$0")\/ratchet.sh"\n. "\$(dirname "\$0")\/husky.sh"/g' $rootDir/.git/hooks/pre-commit
 # move into hooks folder
 cp $rootDir/bin/ratchet/ratchet.sh $rootDir/.git/hooks/ratchet.sh

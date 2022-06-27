406f9132048872b2c0c741d8e14c41eeb1c8dc9d
Michael Gilardi <mgilardi@asu.edu>
2022-06-27 12:34:52 -0700

    Additions to Aliusito's Last Stand

--- bin/aliusito/aliusito_last_stand.sh
+++ bin/aliusito/aliusito_last_stand.sh
@@ -169,13 +169,13 @@ function execute_changes() {
     for branch in $(git for-each-ref --format='%(refname:short)' --sort='*refname:short' refs/heads/); do
         if [[ "$branch" != *\/* ]]; then
 
-            echo -ne "--> \033[1m$branch\033[0m\033[0K\r"
+            echo -ne "--> \033[1m$branch\033[0m"
 
             if [[ $(is_protected_branch "$branch") == "1" ]] || [[ $(is_ignored_branch "$branch") == "1" ]] ; then
                 branchesToSkipArray+=("$branch")
                 branchesToSkipCount=$((branchesToSkipCount + 1))
                 
-                echo " [SKIP]"
+                echo -ne " [SKIP]\033[0K\r"
 
                 if [[ $VERBOSE -eq 1 ]]; then
                     echo "--> $branch is listed as protected or ignored, skipping"

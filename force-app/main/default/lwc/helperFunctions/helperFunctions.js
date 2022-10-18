2739802028f6b509bbf487e02b827c995160d42d
Robert Nordman <rnordman@asu.edu>
2022-10-18 16:09:24 -0700

    Bool parsing helper function

--- force-app/main/default/lwc/helperFunctions/helperFunctions.js
+++ force-app/main/default/lwc/helperFunctions/helperFunctions.js
@@ -47,3 +47,17 @@ export function falseWireRun(obj) {
 
     return data == null && error == null;
 }
+
+/**
+ * Gets the boolean equilvalent of a passed object
+ * Useful for any boolean @api params, since we want to allow
+ * specifying them in the parent as a string
+ *
+ * @param {*} obj Parse this JS item as a boolean
+ * @returns TRUE when either the obj is a string with value "true" or "TRUE" or when the obj is a boolean with value true
+ */
+export function parseBoolean(obj) {
+    if (typeof obj === 'boolean') return obj;
+    else if (typeof obj === 'string') return obj === 'true' || obj === 'TRUE';
+    else return false;
+}

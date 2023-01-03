0029c46f51c0a6b19edf624dbac4d7ca949017c8
Robert Nordman <rnordman@asu.edu>
2023-01-03 14:34:51 -0700

    fix tests name

--- force-app/main/default/lwc/lightningDesignToast/_tests__/lightningDesignToast.test.js
+++ force-app/main/default/lwc/lightningDesignToast/_tests__/lightningDesignToast.test.js
@@ -2,7 +2,7 @@
 import {createElement} from 'lwc';
 import LightningDesignToast from 'c/lightningDesignToast';
 
-describe('c-lightning-datatable-pagination', () => {
+describe('c-lightning-design-toast', () => {
     afterEach(() => {
         // The jsdom instance is shared across test cases in a single file so reset the DOM
         while (document.body.firstChild) {

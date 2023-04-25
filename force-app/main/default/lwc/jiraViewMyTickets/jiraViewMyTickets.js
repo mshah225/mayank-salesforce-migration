9037ac1e4f57a1f5f596207b5363f947818e56d5
Robert Nordman <rnordman@asu.edu>
2023-04-25 10:45:20 -0700

    Configure Testing and TR forms using admin-exposed config

--- force-app/main/default/lwc/jiraViewMyTickets/jiraViewMyTickets.js
+++ force-app/main/default/lwc/jiraViewMyTickets/jiraViewMyTickets.js
@@ -1,11 +1,34 @@
 /* eslint-disable no-use-before-define */
-import {LightningElement, wire} from 'lwc';
+import {LightningElement, wire, api} from 'lwc';
 import getIssuesForCurrentUser from '@salesforce/apex/JiraViewAllIssuesController.getIssuesForCurrentUser';
 import getWatchedIssuesForCurrentUser from '@salesforce/apex/JiraViewAllIssuesController.getWatchedIssuesForCurrentUser';
 
 export default class JiraViewMyTickets extends LightningElement {
     myTickets = [];
     watchedTickets = [];
+    testFormQuestionList = [];
+    techReviewQuestionList = [];
+
+    @api set testFormQuestionListJSON(val) {
+        if (val != null && val !== '') this.testFormQuestionList = JSON.parse(val);
+        else this.testFormQuestionList = [];
+
+        for (let i = 0; i < this.testFormQuestionList.length; i++) this.testFormQuestionList[i].key = 'test-key-' + i;
+    }
+    get testFormQuestionListJSON() {
+        return this.testFormQuestionList;
+    }
+
+    @api set techReviewQuestionListJSON(val) {
+        if (val != null && val !== '') this.techReviewQuestionList = JSON.parse(val);
+        else this.techReviewQuestionList = [];
+
+        for (let i = 0; i < this.techReviewQuestionList.length; i++)
+            this.techReviewQuestionList[i].key = 'tech_review-key-' + i;
+    }
+    get techReviewQuestionListJSON() {
+        return this.techReviewQuestionList;
+    }
 
     @wire(getIssuesForCurrentUser)
     gotIssuesForUser(result) {

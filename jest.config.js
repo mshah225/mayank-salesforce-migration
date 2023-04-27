a3d37c9b27e1e3b69bf6edb518f615e36630eaf5
Robert Nordman <rnordman@asu.edu>
2023-04-27 09:08:35 -0700

    Jest needs mocking files to let us test modal containing components

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,11 @@ const {jestConfig} = require('@salesforce/sfdx-lwc-jest/config');
 module.exports = {
     ...jestConfig,
     modulePathIgnorePatterns: ['<rootDir>/.localdevserver'],
+    // Maps mock test items
+    moduleNameMapper: {
+        '^lightning/modal$': '<rootDir>/force-app/main/default/lwc/__jestMocks__/lightning/modal',
+        '^lightning/modalHeader$': '<rootDir>/force-app/main/default/lwc/__jestMocks__/lightning/modalHeader',
+        '^lightning/modalBody$': '<rootDir>/force-app/main/default/lwc/__jestMocks__/lightning/modalBody',
+        '^lightning/modalFooter$': '<rootDir>/force-app/main/default/lwc/__jestMocks__/lightning/modalFooter',
+    },
 };

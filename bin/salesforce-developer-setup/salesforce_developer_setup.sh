047a230506128911d32ef3d6e0cfe5efaa0cc90c
Michael Gilardi <mgilardi@asu.edu>
2023-07-24 09:57:09 -0700

    Update local settings

--- bin/salesforce-developer-setup/salesforce_developer_setup.sh
+++ bin/salesforce-developer-setup/salesforce_developer_setup.sh
@@ -13,8 +13,8 @@ fi
 npm list --location=global | grep sfdx-cli || npm install --location=global sfdx-cli
 npm list --location=global | grep prettier || npm install --location=global -D -E prettier
 npm list --location=global | grep prettier-plugin-apex || npm install --location=global -D -E prettier-plugin-apex
-npm list | grep prettier || npm install --save-dev --save-exact prettier
-npm list | grep prettier-plugin-apex || npm install --save-dev --save-exact prettier-plugin-apex
+npm list | grep prettier || npm install --save-dev prettier && npm install --save-dev --save-exact prettier
+npm list | grep prettier-plugin-apex || npm install --save-dev prettier-plugin-apex && npm install --save-dev --save-exact prettier-plugin-apex
 npm list | grep eslint || npm install --save-dev eslint
 npm list | grep @babel/core || npm install --save-dev @babel/core
 npm list | grep @babel/eslint-parser || npm install --save-dev @babel/eslint-parser
@@ -26,6 +26,7 @@ npm update --location=global
 
 # Force Visual Studio Code to install or update the extensions
 code --install-extension salesforce.salesforcedx-vscode --force
+code --install-extension salesforce.salesforcedx-vscode-expanded --force
 code --install-extension dbaeumer.vscode-eslint --force
 code --install-extension esbenp.prettier-vscode --force
 code --install-extension fabiospampinato.vscode-commands --force

7f34b4df4dc7fe342752b1fd0893d666677212b9
Robert Nordman <rnordman@asu.edu>
2023-08-22 10:15:45 -0700

    Fix jest.config from merge

--- jest.config.js
+++ jest.config.js
@@ -21,12 +21,5 @@ module.exports = {
         '^lightning/modalBody$': '<rootDir>/force-app/main/default/lwc/__jestMocks__/lightning/modalBody',
         '^lightning/modalFooter$': '<rootDir>/force-app/main/default/lwc/__jestMocks__/lightning/modalFooter',
     },
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver'],
-    // Maps mock test items
-    moduleNameMapper: {
-        '^lightning/modal$': '<rootDir>/force-app/main/default/lwc/__jestMocks__/lightning/modal',
-        '^lightning/modalHeader$': '<rootDir>/force-app/main/default/lwc/__jestMocks__/lightning/modalHeader',
-        '^lightning/modalBody$': '<rootDir>/force-app/main/default/lwc/__jestMocks__/lightning/modalBody',
-        '^lightning/modalFooter$': '<rootDir>/force-app/main/default/lwc/__jestMocks__/lightning/modalFooter',
-    },
+    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
 };

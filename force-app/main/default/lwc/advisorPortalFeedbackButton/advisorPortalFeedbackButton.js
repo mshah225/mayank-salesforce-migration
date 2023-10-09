2bcc8021f10cd7dd0e8774451acd8d85f01c1c1c
vgnshiyer <vgnshiyer@asu.edu>
2023-10-09 14:14:54 -0700

    chore: removed mallicious line from advisorPortalFeedbackButton.js

--- force-app/main/default/lwc/advisorPortalFeedbackButton/advisorPortalFeedbackButton.js
+++ force-app/main/default/lwc/advisorPortalFeedbackButton/advisorPortalFeedbackButton.js
@@ -1,6 +1,5 @@
 import {LightningElement} from 'lwc';
 import submitFeedback from '@salesforce/apex/FeedbackButtonService.submitFeedback';
-import createTicket from '@salesforce/apex/JiraCallout.createTicket';
 import LightningQuestionAnswerModal from 'c/lightningQuestionAnswerModal';
 
 export default class AdvisorPortalFeedbackButton extends LightningElement {

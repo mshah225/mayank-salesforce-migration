46dfa36f762ad75f1b02e80fbd32ff7ad7c92e25
jdsanch8 <jdsanch8@asu.edu>
2024-01-04 18:32:34 -0700

    add logic and details for checkin page

--- force-app/main/default/lwc/campusVisitCheckInForm/campusVisitCheckInForm.js
+++ force-app/main/default/lwc/campusVisitCheckInForm/campusVisitCheckInForm.js
@@ -1,10 +1,43 @@
-import { api } from 'lwc';
+import { LightningElement, wire, track, api } from 'lwc';
 import LightningModal from 'lightning/modal';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi'
+
+import getCampaignMembers from "@salesforce/apex/CampusVisitCheckInController.getCampaignMember"
+import campaignMemberStatusPicklist from '@salesforce/schema/CampaignMember.Status'
 
 export default class CampusVisitCheckInForm extends LightningModal {
-    @api content;
+    @api content;    
+
+    campMem = {};
+    memberStatusPicklistValues = [];
+
+    @wire(getCampaignMembers, {recordId: '$content'})
+    campaignMember(result) {
+        this.campMem = result.data;
+    }
+
+    @wire(getPicklistValues, {
+        recordTypeId: '012d0000000sXUVAA2',
+        fieldApiName: campaignMemberStatusPicklist,
+      })
+      getPicklistValuesForField({ data, error }) {
+        if (error) {
+          // TODO: Error handling
+          console.error(error)
+        } else if (data) {
+          this.memberStatusPicklistValues = [...data.values]
+        }
+      }
+
+    get campaignMemberName() {
+        return this.campMem?.data?.Name;
+    }
 
     handleOkay() {
+        console.log(this.content);
+        console.log(this.campMem);
+        console.log(this.campMem.data);
+        console.log(this.campMem.data.Name);
         this.close('okay');
     }
 }

2cf5518427fe4ca747c9def6a29945522bbfe4c8
Robert Nordman <rnordman@asu.edu>
2024-04-22 10:42:39 -0700

    Pass gradMode flag to mass close modal

--- force-app/main/default/lwc/advisorPortalA/advisorPortalA.js
+++ force-app/main/default/lwc/advisorPortalA/advisorPortalA.js
@@ -472,6 +472,7 @@ export default class AdvisorPortalA extends LightningElement {
             AdvisorPortalModalMassClose.open({
                 size: 'medium',
                 description: 'Close all selected cases',
+                gradMode: this.currentFilter.career === 'GRD',
                 selectedContactWrappers: this.selectedResults,
                 loadingCb: (e) => {
                     this.handleLoading(e);

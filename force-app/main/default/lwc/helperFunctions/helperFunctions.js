8a293d4cf6c1541f6af6c67f1c1c0c73193ff8c5
Robert Nordman <rnordman@asu.edu>
2024-04-22 10:31:26 -0700

    Add helper function to extra errors of various shapes

--- force-app/main/default/lwc/helperFunctions/helperFunctions.js
+++ force-app/main/default/lwc/helperFunctions/helperFunctions.js
@@ -111,3 +111,80 @@ export function getFixedYOffset(attachRoot) {
     }
     return fixedYOffset;
 }
+
+/**
+ * Extract a list of human readable error messages from an error object or list or error objects
+ * @param {Object|List<Object>} errors
+ * @returns String[]
+ */
+export function extractErrorMessages(errors) {
+    if (!Array.isArray(errors)) {
+        errors = [errors];
+    }
+
+    return (
+        errors
+            // Remove null/undefined items
+            .filter((error) => !!error)
+            // Extract an error message
+            .map((error) => {
+                // Wrrors that aren't wrapped in an Error object (maybe from JS throw command)
+                if (typeof error === 'string') {
+                    return error;
+                }
+                // The vast majority of error messages are proper objects - so try to detect object shape to extract error messages
+                else if (typeof error === 'object') {
+                    // UI API read errors
+                    if (Array.isArray(error.body)) {
+                        return error.body.map((e) => e.message);
+                    }
+                    // UI API DML, Apex, missing cacheable=true, network errors
+                    else if (error.body && typeof error.body.message === 'string') {
+                        return error.body.message;
+                    }
+                    // DML field errors (such as invalid lookup to wrong object type)
+                    // or validation errors can cause this
+                    else if (
+                        error.body &&
+                        typeof error.body.fieldErrors === 'object' &&
+                        Object.keys(error.body.fieldErrors).length > 0
+                    ) {
+                        const keys = Object.keys(error.body.fieldErrors);
+                        const allErrors = [];
+                        for (let i = 0; i < keys.length; i++) {
+                            const key = keys[i];
+                            const val = error.body.fieldErrors[key];
+                            allErrors.push(val.map((e) => e.message));
+                        }
+                        return allErrors.reduce((prev, curr) => prev.concat(curr + ' '), '');
+                    }
+                    // page errors (such as mixed DML error)
+                    else if (error.body && Array.isArray(error.body.pageErrors) && error.body.pageErrors.length > 0) {
+                        const pageErrors = error.body.pageErrors;
+                        const allErrors = [];
+                        pageErrors.forEach((e) => {
+                            allErrors.push(e.message);
+                        });
+                        return allErrors.reduce((prev, curr) => prev.concat(curr + ' '), '');
+                    }
+                    // JS errors
+                    else if (typeof error.message === 'string') {
+                        return error.message;
+                    }
+                }
+                // Unknown error shape so try HTTP status text
+                return error.statusText;
+            })
+            // Flatten
+            .reduce((prev, curr) => prev.concat(curr), [])
+            // Remove empty strings
+            .filter((message) => !!message)
+            .map((message) => message.trim())
+    );
+}
+
+// Helper function to wait until the microtask queue is empty. This is needed for promise
+// timing when calling imperative Apex.
+export async function flushPromises() {
+    return Promise.resolve();
+}
